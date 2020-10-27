390425ed1db43fde7fb1d4d52402e3fee4ef30f3
Dmitry Diachenko <44314830+Fenki93@users.noreply.github.com>
2020-10-27 15:05:52 +0100

    changes to #5

--- homework2.py
+++ homework2.py
@@ -68,17 +68,9 @@ for i, el in enumerate(word, 1):
 
 my_list = [7, 5, 3, 3, 2]
 number = int(input("Enter number: "))
-c = my_list.count(number)
-for element in my_list:
-    if c > 0:
-        i = my_list.index(number)
-        my_list.insert(i+c, number)
-        break
-    else:
-        if number > element:
-            j = my_list.index(element)
-            my_list.insert(j, number)
-            break
-        elif number < my_list[len(my_list) - 1]:
-            my_list.append(number)
+index = 0
+
+for item in my_list:
+    if new_number <= item:
+        index = my_list.index(item) + 1
 print(my_list)
